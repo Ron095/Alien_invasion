a658dc4df30b2b0049b160e6839c04bae07ba6e8
Ronald Rodriguez <ronrf095@hotmail.com>
2023-03-10 17:19:12 -0500

    adding changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 def run_game():
     #Initialize game and create a screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #Make a ship.
+    ship = Ship(screen)
 
     #Start the main loop for the game.
     while True:
@@ -16,6 +20,7 @@ def run_game():
                 sys.exit()
         #Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitime()
 
         #Make the most recently drawn screen visible
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -1,6 +1,6 @@
 import pygame
 class Ship():
-    def __int__(self, screen):
+    def __init__(self, screen):
         #Initialize the ship and set its starting position
         self.screen = screen
 
@@ -10,3 +10,9 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         #Start each new ship at the bottom center of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitime(self):
+        #Draw the ship at its current location
+        self.screen.blit(self.image, self.rect)
