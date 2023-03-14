2d4a892bbbb21d0cf645d2929a9fe0ed55c9deae
Ronald Rodriguez <ronrf095@hotmail.com>
2023-03-14 08:51:18 -0500

    New Changes

--- ship.py
+++ ship.py
@@ -9,9 +9,17 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
+        #Movement flag
+        self.moving_right = False
+
         #Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+    def update(self):
+        #Update the ship's position based on the movement flag
+        if self.moving_right:
+            self.rect.centerx =+ 1
 
     def blitime(self):
         #Draw the ship at its current location
